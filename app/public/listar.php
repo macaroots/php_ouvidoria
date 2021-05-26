701049b7da44870fdbaed8191b3eb43c347a03a9
Renato <macaroots@gmail.com>
2021-05-26 18:35:55 -0300

    Visual em tabela

--- app/public/listar.php
+++ app/public/listar.php
@@ -12,4 +12,23 @@ if ($consulta->execute()) {
 }
 
 // visao
-echo json_encode($lista);
+?>
+<html>
+<style>
+tr:nth-child(even) {background: #CCC}
+</style>
+<body>
+    <table>
+<?php
+foreach ($lista as $linha) {
+?>
+        <tr>
+            <td><?php echo $linha['data']; ?></td>
+            <td><?php echo $linha['comentario']; ?></td>
+        </tr>
+<?php
+}
+?>
+    </table>
+</body>
+</html>
